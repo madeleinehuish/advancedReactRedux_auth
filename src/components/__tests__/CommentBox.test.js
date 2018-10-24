43e9a81237de616604f2724feee17c1e4f88acfa
Madeleine Huish <madeleinehuish@gmail.com>
2018-10-24 13:07:02 -0700

    wiring up redux

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,25 @@ it('has a text area and a button', () => {
 	expect(wrapped.find('textarea').length).toEqual(1);
 	expect(wrapped.find('button').length).toEqual(1);
 })
+
+describe('the text area', () => {
+
+	beforeEach(() => {
+		wrapped.find('textarea').simulate('change', { target: { value: 'new comment' } });
+		wrapped.update(); //forces component to update (required as the setState change above happens asynchronously)
+	})
+
+	it('has a text area that users can type in', () => {
+		expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+	})
+
+	it('when form is submitted, textarea gets emptied', () => {
+		expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+
+		wrapped.find('form').simulate('submit');
+		wrapped.update();
+
+		expect(wrapped.find('textarea').prop('value')).toEqual('');
+	})
+
+})
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from 'reducers'; //using absolute urls (see .env), will go straight to index when you use folder
 
 import App from 'components/App';
 
 ReactDOM.render(
-	<App />,
+	<Provider store={createStore(reducers, {})}>
+		<App />
+	</Provider>,
 	document.querySelector('#root')
 );
