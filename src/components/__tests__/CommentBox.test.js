06c96623de5ff98bc8d80d7fefc5f043bc760fdb
Madeleine Huish <madeleinehuish@gmail.com>
2018-10-23 22:11:20 -0700

    clean up

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -8,12 +8,11 @@ beforeEach(() => {
 	wrapped = mount(<CommentBox />);
 })
 
-it('has a text area and a button', () => {
-
-	expect(wrapped.find('textarea').length).toEqual(1);
-	expect(wrapped.find('button').length).toEqual(1);
-})
-
 afterEach(() => {
 	wrapped.unmount();
+})
+
+it('has a text area and a button', () => {
+	expect(wrapped.find('textarea').length).toEqual(1);
+	expect(wrapped.find('button').length).toEqual(1);
 })
