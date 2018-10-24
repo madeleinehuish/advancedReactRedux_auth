f227ec80af4d06852147f27ae511cbdbc22f5369
Madeleine Huish <madeleinehuish@gmail.com>
2018-10-23 21:12:30 -0700

    second commit

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,15 +4,16 @@ import App from 'components/App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
+let wrapped;
+
+beforeEach(() => {
+	wrapped = shallow(<App />);
+})
+
 it('shows a comment box', () => {
-	const wrapped = shallow(<App />);
-
 	expect(wrapped.find(CommentBox).length).toEqual(1); //wrapped.find returns array so we check length as we only need one instance
-
 })
 
 it('shows a comment list', () => {
-	const wrapped = shallow(<App />);
-
 	expect(wrapped.find(CommentList).length).toEqual(1);
 })
