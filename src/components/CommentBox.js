fd1b63f291580667900596f373c9fec82138aab1
Madeleine Huish <madeleinehuish@gmail.com>
2018-10-30 10:52:41 -0700

    finished with higher order component section

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from 'actions';
+import requireAuth from 'components/requireAuth';
 
 class CommentBox extends Component {
 
@@ -34,4 +35,4 @@ class CommentBox extends Component {
 	}
 }
 
-export default connect(null, actions)(CommentBox);
+export default connect(null, actions)(requireAuth(CommentBox));
